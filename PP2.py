4b2c581b55199134856e23b66216dabc3a916a6c
borolasgamer <emeliodeivid@gmail.com>
2022-11-10 13:04:16 -0600

    ayuda pibes

--- PP2.py
+++ PP2.py
@@ -1,19 +1,14 @@
-##EmilioParte/2-multiplicacionDeNNumeros
-# Creamos las listas (vacías al comienzo)
-numeros = []
-# Definimos un tamaño para las listas
-# Lo puedes cambiar si quieres
-tamaño = int(input('Ingrese la cantidad de numeros que quiere multiplicar:'))
-
-# Leemos los datos y los agregamos a la lista
-for i in range(tamaño):
-    print("Ahora podra ingresar el producto", i + 1)
-    listanumeros = int(input(": "))
-    numeros.append(listanumeros)
-    
-
-# Ahora mostremos las listas
-for i in range(tamaño):
-    print("Mostrando los datos de la persona", i + 1)
-
-    print("Nombre:", numeros[i])
+#menu
+class MenuPrincipal:
+    menuprincipal = int(input("Menu Principal: \n 1-Suma de N numeros \n 2-Producto entre N numeros \n 3-Division entre 2 numeros \n 4-calcular el factorial de 1 numero \n 5-Ver tablas de multiplicar \n 6-Calcular el cuadrado y el cubo de 1 numero \n 7-Obtener el promedio de una serie de numeros \n 8-Obtener el valor maximo y minimo de una de una serie de numeros \n 9-Escriba 0 para salir \n"))
+    while menuprincipal !=0:
+        if menuprincipal ==1:
+            print("suma de numeros")
+        elif menuprincipal ==2:
+            print("multiplicacion de numeros")
+        elif menuprincipal ==3:
+            print("Division de numeros")
+        elif menuprincipal ==4:
+            print("Calculo de un factorial")
+        elif menuprincipal ==5:
+            tabla=int(input("seleccione una de las tablas de multiplicar:"))
