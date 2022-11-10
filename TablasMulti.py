be75946906d81f24c122e24091bb7c0d1ef27c36
borolasgamer <emeliodeivid@gmail.com>
2022-11-10 12:59:45 -0600

    Tablas de multiplicar

---
+++ TablasMulti.py
@@ -0,0 +1,6 @@
+
+tablas= int(input('agregue un numero:'))
+if tablas!=1:
+    for f in range(1,11):
+        multiplicacion = tablas * f #hacemos la multiplicación
+        print (tablas,f"x {f} = {multiplicacion}") #mostramos el resultado
