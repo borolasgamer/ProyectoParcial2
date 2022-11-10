2e7f232a9ca4c76df803cadc05a6cbc60d8fcc64
borolasgamer <emeliodeivid@gmail.com>
2022-11-10 10:13:22 -0600

    Ya se creo la lista para almancenar los datos/ Falta multiplicar los datos dentro de la lista

--- PP2.py
+++ PP2.py
@@ -1,7 +1,19 @@
 ##EmilioParte/2-multiplicacionDeNNumeros
-import math
-lista.sort()
-print("Tamaño de la lista: ", len(lista))
-print("Lista ordenada ")
-print(lista)
+# Creamos las listas (vacías al comienzo)
+numeros = []
+# Definimos un tamaño para las listas
+# Lo puedes cambiar si quieres
+tamaño = int(input('Ingrese la cantidad de numeros que quiere multiplicar:'))
 
+# Leemos los datos y los agregamos a la lista
+for i in range(tamaño):
+    print("Ahora podra ingresar el producto", i + 1)
+    listanumeros = int(input(": "))
+    numeros.append(listanumeros)
+    
+
+# Ahora mostremos las listas
+for i in range(tamaño):
+    print("Mostrando los datos de la persona", i + 1)
+
+    print("Nombre:", numeros[i])
